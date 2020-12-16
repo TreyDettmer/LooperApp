ee6c9b297ff555ae57d2ca5654d0dc4425dbe951
TreyDettmer <dettmertrey@gmail.com>
2020-12-15 22:52:19 -0800

    added project

---
+++ LooperApp/Assets/Scripts/Looper.cs
@@ -0,0 +1,395 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Looper : MonoBehaviour
+{
+    public Slider progressBar;
+    public Image looperBackgroundImage;
+    public Image bpmWarningImage;
+    public Button playButton;
+
+    public AudioSource audioSource0;
+    public AudioSource audioSource1;
+    int frequency = 44100;
+
+    double playbackBuffer = .15f;
+
+
+    double recordingStartTime = 100f;
+    double recordingFirstBeatTime = 200f;
+
+    double firstClipStartTime = 0f;
+    double secondClipStartTime = 0f;
+    bool bIsRecording = false;
+    bool bHasClip = false;
+    bool bPaused = true;
+    int clipIndex = 0;
+    bool bScheduledFirstClip = true;
+    bool bScheduledSecondClip = false;
+    double numberOfMeasures = 0;
+    double recordedBPM = 120;
+
+    Coroutine switchCoroutine0;
+    Coroutine switchCoroutine1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+        if (Microphone.devices.Length <= 0)
+        {
+            Debug.LogWarning("Microphone not connected.");
+            return;
+        }
+        playButton.enabled = false;
+        bpmWarningImage.enabled = false;
+    }
+
+
+
+    public void Record()
+    {
+        
+        bIsRecording = !bIsRecording;
+        if (bIsRecording == true)
+        {
+            if (Station.instance.bRecordingALoop) {
+                bIsRecording = false;
+                return; 
+            }
+            recordedBPM = Station.instance.bpm;
+            Station.instance.bRecordingALoop = true;
+            audioSource0.Stop();
+            audioSource1.Stop();
+            Microphone.End(null);
+            audioSource0.clip = null;
+            audioSource1.clip = null;
+            //start recording
+            audioSource0.clip = Microphone.Start(null, true, 40, frequency);
+            recordingStartTime = AudioSettings.dspTime;
+            if (Station.instance.recordingCountIn == 1)
+            {
+                recordingFirstBeatTime = Station.instance.nextDownBeatTime;
+            }
+            else if (Station.instance.recordingCountIn == 2)
+            {
+                recordingFirstBeatTime = Station.instance.nextDownBeatTime + Station.instance.timeBetweenDownBeats;
+            }
+            else
+            {
+                recordingFirstBeatTime = Station.instance.nextDownBeatTime + 2 * Station.instance.timeBetweenDownBeats;
+            }
+            
+            StartCoroutine(ChangeLooperBackgroundRoutine(recordingFirstBeatTime - recordingStartTime));
+
+
+
+
+        }
+        else
+        {
+            
+            looperBackgroundImage.color = new Color(173f/255f, 173f/255f, 173f/255f, 222f/255f);
+            if (audioSource0.clip == null) { return; }
+            double t0 = AudioSettings.dspTime;
+            //stop recording and store recorded data into audio clip
+            int uncutLength = Microphone.GetPosition(null);
+            
+            Microphone.End(null);
+            float[] uncutClipData = new float[uncutLength];
+            audioSource0.clip.GetData(uncutClipData, 0);
+            double endTime = AudioSettings.dspTime;
+            //Debug.Log($"Uncut samples: {uncutLength} Uncut Time: {uncutLength / frequency} seconds");
+            if (recordingFirstBeatTime - recordingStartTime < 0)
+            {
+                Debug.LogError("Recording somehow started after the downbeat.");
+            }
+            double beginningTimeToDiscard = recordingFirstBeatTime - recordingStartTime;
+            //Debug.Log($"Beginning Time to discard: {(beginningTimeToDiscard).ToString("F4")} seconds");
+            int beginningSamplesToDiscard = Mathf.RoundToInt((float)(frequency * beginningTimeToDiscard));
+            //Debug.Log($"Beginning Samples to discard: {beginningSamplesToDiscard} Beginning Time to discard: {(beginningTimeToDiscard).ToString("F4")} seconds");
+            //Debug.Log($"PreviousDownBeatTime: {Station.instance.previousDownBeatTime} EndTime: {endTime}");
+            double endTimeToDiscard;
+            if (Station.instance.recordingCountOut == 1)
+            {
+                endTimeToDiscard = endTime - Station.instance.previousDownBeatTime;
+            }
+            else if (Station.instance.recordingCountOut == 2)
+            {
+                endTimeToDiscard = endTime - (Station.instance.previousDownBeatTime - Station.instance.timeBetweenDownBeats);
+            }
+            else
+            {
+                endTimeToDiscard = endTime - (Station.instance.previousDownBeatTime - 2 * Station.instance.timeBetweenDownBeats);
+            }
+            if (endTimeToDiscard < 0)
+            {
+                Debug.LogWarning("Failed to create clip: Did not record long enough.");
+                Station.instance.bRecordingALoop = false;
+                return;
+            }
+            int endSamplesToDiscard = Mathf.Max(0,Mathf.RoundToInt((float)(frequency * endTimeToDiscard)) - 400);
+            //Debug.Log($"CurrentTime: {AudioSettings.dspTime} PreviousDownBeatTime: {Station.instance.previousDownBeatTime}");
+            //Debug.Log($"End samples to discard: {endSamplesToDiscard}  End Time to discard: {(endTimeToDiscard).ToString("F4")} seconds");
+
+            double t1 = AudioSettings.dspTime;
+            //Debug.Log($"UncutLength: {uncutLength} BeginningSamplesToDiscard: {beginningSamplesToDiscard} EndSamplesToDiscard: {endSamplesToDiscard}");
+            int clipLength = uncutLength - beginningSamplesToDiscard - endSamplesToDiscard;
+            if (clipLength <= 0)
+            {
+                Debug.LogWarning("Failed to create clip: clip length is less than or equal to zero.");
+                Debug.Log($"SAMPLES: Uncutlength {uncutLength}" +
+                    $" beginningSamplesToDiscard {beginningSamplesToDiscard} endSamplesToDiscard {endSamplesToDiscard} " +
+                    $"CurrentTime {AudioSettings.dspTime} PreviousDownBeatTime {Station.instance.previousDownBeatTime}");
+                Station.instance.bRecordingALoop = false;
+                return;
+            }
+            //Debug.Log(clipLength);
+            float[] clipData = new float[clipLength];
+            
+            for (int i = 0; i < clipLength; i++)
+            {
+                if (beginningSamplesToDiscard + i < uncutLength)
+                {
+                    clipData[i] = uncutClipData[beginningSamplesToDiscard + i];
+                }
+            }
+            double t2 = AudioSettings.dspTime;
+            
+            audioSource0.clip = AudioClip.Create("clip0", clipData.Length, 1, frequency, false);
+            audioSource0.clip.SetData(clipData, 0);
+            audioSource1.clip = AudioClip.Create("clip1", clipData.Length, 1, frequency, false);
+            audioSource1.clip.SetData(clipData, 0);
+            //audioSource.PlayScheduled(Station.instance.nextDownBeatTime);
+            //audioSource.PlayScheduled()
+            numberOfMeasures = Mathf.Ceil((float)((clipData.Length / frequency) / Station.instance.timeBetweenDownBeats));
+            Debug.Log($"NumberOfMeasures: {numberOfMeasures}");
+            for (int i = 1; i <= 16; i++)
+            {
+
+            }
+            double t3 = AudioSettings.dspTime;
+            //Debug.Log($"t0:{t0.ToString("F4")} t1:{t1.ToString("F4")} t2:{t2.ToString("F4")} t3:{t3.ToString("F4")}");
+            bHasClip = true;
+            Station.instance.bRecordingALoop = false;
+            playButton.enabled = true;
+        }
+    }
+
+    IEnumerator ChangeLooperBackgroundRoutine(double wait)
+    {
+        yield return new WaitForSeconds((float)wait);
+        looperBackgroundImage.color = new Color(1f, 0f, 0f, 222f / 255f);
+    }
+
+    public void Playback()
+    {
+        if (bHasClip)
+        {
+            if (bPaused)
+            {
+                bPaused = false;
+                clipIndex = 0;
+                if (switchCoroutine0 != null)
+                {
+                    StopCoroutine(switchCoroutine0);
+                }
+                if (switchCoroutine1 != null)
+                {
+                    StopCoroutine(switchCoroutine1);
+                }
+                audioSource0.PlayScheduled(Station.instance.nextDownBeatTime - playbackBuffer);
+                firstClipStartTime = Station.instance.nextDownBeatTime - playbackBuffer;
+                StartCoroutine(PlaybackLoop());
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+    }
+
+    void Pause()
+    {
+        bPaused = true;
+        if (switchCoroutine0 != null)
+        {
+            StopCoroutine(switchCoroutine0);
+        }
+        if (switchCoroutine1 != null)
+        {
+            StopCoroutine(switchCoroutine1);
+        }
+        audioSource0.Stop();
+        audioSource1.Stop();
+        bScheduledFirstClip = false;
+        bScheduledSecondClip = false;
+        
+    }
+    
+    IEnumerator PlaybackLoop()
+    {
+        while (bHasClip && !bPaused)
+        {
+            if (clipIndex == 0)
+            {
+                //if the first clip has started playing
+                if (AudioSettings.dspTime - firstClipStartTime >= 0.1)
+                {
+
+                    if (bScheduledSecondClip == false)
+                    {
+                        audioSource0.SetScheduledEndTime(Station.instance.nextDownBeatTime + Station.instance.timeBetweenDownBeats * numberOfMeasures);
+                        audioSource1.PlayScheduled(Station.instance.nextDownBeatTime + (Station.instance.timeBetweenDownBeats * numberOfMeasures) - playbackBuffer);
+                        //if (bInitializedSecondClip)
+                        //{
+                        //    audioSource1.SetScheduledStartTime(Station.instance.nextDownBeatTime - .15);
+                        //}
+                        //else
+                        //{
+                        //    audioSource1.PlayScheduled(Station.instance.nextDownBeatTime - .15);
+                        //    bInitializedSecondClip = true;
+                        //}
+                            
+                        secondClipStartTime = Station.instance.nextDownBeatTime + (Station.instance.timeBetweenDownBeats * numberOfMeasures) - playbackBuffer;
+                        bScheduledSecondClip = true;
+                        bScheduledFirstClip = false;
+                        switchCoroutine1 = StartCoroutine(SwitchClipIndex((float)(secondClipStartTime - AudioSettings.dspTime)));
+
+                    }
+                    
+                }
+            }
+            else
+            {
+                //if the second clip has started playing
+                if (AudioSettings.dspTime - secondClipStartTime >= 0.1)
+                {
+
+                    if (bScheduledFirstClip == false)
+                    {
+                        audioSource1.SetScheduledEndTime(Station.instance.nextDownBeatTime + Station.instance.timeBetweenDownBeats * numberOfMeasures);
+                        //audioSource0.SetScheduledStartTime(Station.instance.nextDownBeatTime - .15);
+                        audioSource0.PlayScheduled(Station.instance.nextDownBeatTime + (Station.instance.timeBetweenDownBeats * numberOfMeasures) - playbackBuffer);
+                            
+                        firstClipStartTime = Station.instance.nextDownBeatTime + (Station.instance.timeBetweenDownBeats * numberOfMeasures) - playbackBuffer;
+                        bScheduledFirstClip = true;
+                        bScheduledSecondClip = false;
+                        switchCoroutine0 = StartCoroutine(SwitchClipIndex((float)(firstClipStartTime - AudioSettings.dspTime)));
+                        
+                            
+                    }
+                    
+                }
+            }
+            yield return null;
+        }
+    }
+    //IEnumerator PlaybackStartDelay()
+    //{
+    //    yield return new WaitUntil(() => AudioSettings.dspTime - Station.instance.previousDownBeatTime <= .05f);
+
+    //    //int samplesToSkipOver = Mathf.RoundToInt(frequency * (AudioSettings.dspTime - Station.instance.previousDownBeatTime));
+    //    //audioSource.clip.
+    //    //if (bHasClip && !audioSource.isPlaying)
+    //    //{
+    //    //    audioSource.Play();
+    //    //}
+    //}
+
+
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(AudioSettings.dspTime);
+        if (Microphone.IsRecording(null) && bIsRecording)
+        {
+            if (AudioSettings.dspTime >= recordingFirstBeatTime)
+            {
+                progressBar.value = (float)(AudioSettings.dspTime - recordingFirstBeatTime) / 40;
+            }
+
+        }
+        else
+        {
+            if (clipIndex == 0 && bHasClip)
+            {
+                int timeSamples = audioSource0.timeSamples;
+                int clipLength = audioSource0.clip.samples;
+                float percent = (float)timeSamples / clipLength;
+                progressBar.value = percent;
+            }
+            if (clipIndex == 1 && bHasClip)
+            {
+                int timeSamples = audioSource1.timeSamples;
+                int clipLength = audioSource1.clip.samples;
+                float percent = (float)timeSamples / clipLength;
+                progressBar.value = percent;
+            }
+            if (!audioSource0.isPlaying && !audioSource1.isPlaying)
+            {
+                if (progressBar.value != 0)
+                {
+                    progressBar.value = 0;
+                }
+            }
+        }
+
+    }
+
+    public IEnumerator SwitchClipIndex(float timeToWait)
+    {
+        yield return new WaitForSeconds(timeToWait);
+        if (!bPaused)
+        {
+            clipIndex = 1 - clipIndex;
+        }
+    }
+    public void ClearLoop()
+    {
+        bHasClip = false;
+        bIsRecording = false;
+        playButton.enabled = false;
+        audioSource0.Stop();
+        audioSource1.Stop();
+        if (switchCoroutine0 != null)
+        {
+            StopCoroutine(switchCoroutine0);
+        }
+        if (switchCoroutine1 != null)
+        {
+            StopCoroutine(switchCoroutine1);
+        }
+        bScheduledFirstClip = false;
+        bScheduledSecondClip = false;
+        audioSource0.clip = null;
+        audioSource1.clip = null;
+        bpmWarningImage.enabled = false;
+    }
+
+    public void UpdatedBPM()
+    {
+        if (Station.instance.bpm != recordedBPM && bHasClip)
+        {
+            bpmWarningImage.enabled = true;
+        }
+        else
+        {
+            bpmWarningImage.enabled = false;
+        }
+    }
+
+    public void DeleteLooper()
+    {
+        ClearLoop();
+        Station.instance.loopers.Remove(this);
+        Destroy(gameObject);
+    }
+}
---
+++ LooperApp/Assets/Scripts/Station.cs
@@ -0,0 +1,302 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class Station : MonoBehaviour
+{
+
+    public static Station instance;
+
+
+    [HideInInspector]
+    public double bpm = 140.0F;
+    [HideInInspector]
+    public float gain = 0.5F;
+    [HideInInspector]
+    public int signatureHi = 4;
+    [HideInInspector]
+    public int signatureLo = 4;
+    [HideInInspector]
+    public double nextTick = 0.0F;
+    private double sampleRate = 0.0F;
+    [HideInInspector]
+    public int accent;
+    private bool running = false;
+    [HideInInspector]
+    public double startTick;
+    public double nextDownBeatTime;
+    [HideInInspector]
+    public double nextUpBeatTime;
+    public double currentDownBeatTime;
+    public double currentTime;
+    public double previousDownBeatTime;
+    [HideInInspector]
+    public double currentUpBeatTime;
+    [HideInInspector]
+    public double previousUpBeatTime;
+
+    public AudioSource metronomeLow;
+    public AudioSource metronomeHigh;
+    public TextMeshProUGUI countInButtonText;
+    public TextMeshProUGUI countOutButtonText;
+    public GameObject looperPrefab;
+    public Transform backgroundPanel;
+    public int beatCount = 0;
+
+    public bool bRecordingALoop = false;
+
+    public double timeBetweenDownBeats;
+
+    public int recordingCountIn = 1;
+    public int recordingCountOut = 1;
+
+
+    public TMP_InputField bpmInputField;
+    [HideInInspector]
+    public bool bUseMetronome = true;
+
+    bool bScheduledNextDownBeatTime = false;
+    bool bScheduledNextUpBeatTime = false;
+
+    public List<Looper> loopers = new List<Looper>();
+
+    /// <summary>
+    /// Initialize singleton
+    /// </summary>
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+
+        }
+        else if (instance != this)
+        {
+            Destroy(this);
+        }
+    }
+
+    void Start()
+    {
+        //initialize metronome values
+        accent = signatureHi;
+        double startTick = AudioSettings.dspTime;
+        sampleRate = AudioSettings.outputSampleRate;
+        nextTick = startTick * sampleRate;
+        nextDownBeatTime = AudioSettings.dspTime + 2;
+        nextUpBeatTime = nextDownBeatTime;
+        timeBetweenDownBeats = (60 * signatureHi) / bpm;
+        Looper[] loopersToAdd = FindObjectsOfType<Looper>();
+        foreach(Looper looper in loopersToAdd)
+        {
+            if (!loopers.Contains(looper))
+            {
+                loopers.Add(looper);
+            }
+        }
+        running = true;
+    }
+
+    private void Update()
+    {
+        if (!running)
+        {
+            return;
+        }
+        currentTime = AudioSettings.dspTime;
+        double time = AudioSettings.dspTime;
+        if (!bScheduledNextDownBeatTime)
+        {
+            ScheduleNextDownBeatTime();
+        }
+        //if (time + .3f > nextDownBeatTime)
+        //{
+        //    if (bUseMetronome)
+        //    {
+        //        metronomeLow.PlayScheduled(nextDownBeatTime);
+        //    }
+        //    previousDownBeatTime = currentDownBeatTime;
+        //    currentDownBeatTime = nextDownBeatTime;
+        //    nextDownBeatTime += 60.0f / bpm * signatureHi;
+
+        //}
+        if (!bScheduledNextUpBeatTime)
+        {
+            ScheduleNextUpBeatTime();
+        }
+
+        //if (time + .1f > nextUpBeatTime)
+        //{
+
+
+        //    if (beatCount % 4 != 0)
+        //    {
+        //        if (bUseMetronome)
+        //        {
+        //            metronomeHigh.PlayScheduled(nextUpBeatTime);
+        //        }
+        //    }
+        //    previousUpBeatTime = currentUpBeatTime;
+        //    currentUpBeatTime = nextUpBeatTime;
+        //    nextUpBeatTime += 60.0f / (bpm * 4) * signatureHi;
+            
+        //}
+    }
+
+    public void ChangeBPM()
+    {
+        int tempBPM;
+
+        int.TryParse(bpmInputField.text,System.Globalization.NumberStyles.Integer,null, out tempBPM);
+        if (tempBPM > 0)
+        {
+            bpm = tempBPM;
+            timeBetweenDownBeats = (60 * signatureHi) / bpm;
+            nextDownBeatTime = AudioSettings.dspTime + 2.0f;
+            nextUpBeatTime = nextDownBeatTime;
+            beatCount = 0;
+            foreach (Looper looper in loopers)
+            {
+                looper.UpdatedBPM();
+            }
+        }
+
+    }
+
+    public void SetMetronome()
+    {
+        bUseMetronome = !bUseMetronome;
+    }
+
+    public void ScheduleNextDownBeatTime()
+    {
+        bScheduledNextDownBeatTime = true;
+        if (bUseMetronome)
+        {
+            metronomeHigh.PlayScheduled(nextDownBeatTime);
+        }
+        //Debug.Log("Scheduled");
+        Invoke("ResetDownBeatTimeBool",(float)(nextDownBeatTime + .1 - AudioSettings.dspTime));
+    }
+
+    public void ScheduleNextUpBeatTime()
+    {
+        
+        bScheduledNextUpBeatTime = true;
+        
+
+        if (bUseMetronome)
+        {
+            metronomeLow.PlayScheduled(nextUpBeatTime);
+        }
+        
+        //beatCount++;
+        
+        Invoke("ResetUpBeatTimeBool", (float)(nextUpBeatTime + .1 - AudioSettings.dspTime));
+    }
+
+    public void ResetDownBeatTimeBool()
+    {
+        previousDownBeatTime = nextDownBeatTime;
+        nextDownBeatTime += 60.0f / bpm * signatureHi;
+        bScheduledNextDownBeatTime = false;
+        //Debug.Log("Reset");
+    }
+
+    public void ResetUpBeatTimeBool()
+    {
+        
+        previousUpBeatTime = nextUpBeatTime;
+        nextUpBeatTime += 60.0f / (bpm * 4) * signatureHi;
+        bScheduledNextUpBeatTime = false;
+    }
+
+    public void PressedCountInButton()
+    {
+        if (bRecordingALoop) { return; }
+        if (recordingCountIn == 3)
+        {
+            recordingCountIn = 1;
+        }
+        else
+        {
+            recordingCountIn++;
+        }
+        countInButtonText.text = recordingCountIn.ToString();
+    }
+
+    public void PressedCountOutButton()
+    {
+        if (bRecordingALoop) { return; }
+        if (recordingCountOut == 3)
+        {
+            recordingCountOut = 1;
+        }
+        else
+        {
+            recordingCountOut++;
+        }
+        countOutButtonText.text = recordingCountOut.ToString();
+    }
+
+    public void AddLooper()
+    {
+        if (loopers.Count < 5)
+        {
+            Looper newLooper = Instantiate(looperPrefab, backgroundPanel).GetComponent<Looper>();
+            loopers.Add(newLooper);
+        }
+    }
+
+
+    ///// <summary>
+    ///// Generates metronome beeps
+    ///// </summary>
+    ///// <param name="data">audio data</param>
+    ///// <param name="channels">number of channels of audio</param>
+    //void OnAudioFilterRead(float[] data, int channels)
+    //{
+    //    if (!running)
+    //        return;
+
+    //    double samplesPerTick = sampleRate * 60.0F / bpm * 4.0F / signatureLo;
+    //    double sample = AudioSettings.dspTime * sampleRate;
+    //    int dataLen = data.Length / channels;
+        
+    //    int n = 0;
+    //    while (n < dataLen)
+    //    {
+    //        float x = gain * amp * Mathf.Sin(phase);
+    //        int i = 0;
+    //        while (i < channels)
+    //        {
+    //            data[n * channels + i] += x;
+    //            i++;
+    //        }
+            
+    //        while (sample + n >= nextTick)
+    //        {
+    //            nextTick += samplesPerTick;
+    //            amp = 1.0F;
+    //            if (++accent > signatureHi)
+    //            {
+    //                nextDownBeatTime = AudioSettings.dspTime + 4 * (60 / bpm);
+    //                previousDownBeatTime = nextDownBeatTime - 4 * (60 / bpm);
+    //                //Debug.Log($"Current Time: {AudioSettings.dspTime} Beat Time: {previousDownBeatTime}");
+    //                Debug.Log($"Current Beat: {previousDownBeatTime}  Next Beat: {nextDownBeatTime}");
+    //                accent = 1;
+    //                amp *= 2.0F;
+    //            }
+                
+    //            //Debug.Log("Tick: " + accent + "/" + signatureHi);
+    //        }
+    //        phase += amp * 0.3F;
+    //        amp *= 0.993F;
+    //        n++;
+    //    }
+    //}
+
+}
